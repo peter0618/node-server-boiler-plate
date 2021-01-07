43ebe4c4817faf1c9053ac9f91186d6b886f2b1d
peter0618 <peterchoi0618@gmail.com>
2021-01-08 02:01:33 +0900

    로그인 -> 토큰생성 -> 쿠키에 저장 로직 구현

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -46,8 +47,32 @@ userSchema.pre('save', function(next){
                 next();
             });
         })
+    } else {
+        next(); // password 변경이 아닐 때에도 next()를 해줘야 다음 로직이 실행됩니다.
     }
 });
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+
+    //plainPassword 와 암호화된 비밀번호를 비교
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err);
+        cb(null, isMatch);
+    });
+}
+
+userSchema.methods.generateToken = function (cb) {
+    var user = this;
+
+    // jsonwebtoken 을 이용해서 token 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+    user.token = token;
+    user.save(function(err, user) {
+        if(err) return cb(err);
+        cb(null, user);
+    });
+}
 
 const User = mongoose.model('User', userSchema)
 
