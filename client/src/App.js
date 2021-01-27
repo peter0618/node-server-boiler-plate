5c38b8818438c0bd66751e75791360d428867b5a
peter0618 <peterchoi0618@gmail.com>
2021-01-27 22:24:07 +0900

    React Router 셋팅

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router} from "react-router-dom";
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import {Route, Switch} from "react-router";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
 
 function App() {
   return (
-      <div>
-        App
-      </div>
+      <Router>
+        <div>
+            <Switch>
+                <Route path="/" exact={true} component={LandingPage}/>
+                <Route path="/login" exact={true} component={LoginPage}/>
+                <Route path="/register" exact={true} component={RegisterPage}/>
+            </Switch>
+        </div>
+      </Router>
   )
 }
 
