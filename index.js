e3d04fbad673ca3e3c537a2473e0d5103d1c2f2a
peter0618 <peterchoi0618@gmail.com>
2021-02-06 02:03:04 +0900

    몽고디비 docker 설정 파일 추가. body-parser 설정 추가. User 생성 API 추가

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+const {User} = require('./models/User');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/testDB', {
+mongoose.connect('mongodb://peter:1234@localhost:27017/test_db', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => res.send('hello world'));
+
+app.post('/register', (req, res) => {
+
+    const user = new User(req.body);
+    user.save((err,userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success:true
+        })
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
