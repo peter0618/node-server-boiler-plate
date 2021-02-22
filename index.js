58f79e0cd1e444192ffa742d03d4e0340862477a
peter0618 <peterchoi0618@gmail.com>
2021-02-23 00:51:08 +0900

    사용자 인증 구현

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const app = express();
 const port = 5000;
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
 const config = require('./config/key');
-
+const { auth } = require('./middleware/auth');
 const {User} = require('./models/User');
 
 // application/x-www-form-urlencoded
@@ -64,8 +63,18 @@ app.post('/login', (req, res) => {
     });
 });
 
-app.post('/auth', ( req, res) => {
-
+app.get('/api/users/auth', auth, (req, res) => {
+    // 여기까지 미들웨어를 통과했으면, authentication 이 true 라는 말
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true, // role=0 : 일반 유저, role!=0 : 관리자로 가정
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    });
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
